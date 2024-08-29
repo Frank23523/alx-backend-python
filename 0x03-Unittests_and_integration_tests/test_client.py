9e9996d25343b4e02f8a15e4ce139b0b79a242c6
Frank Anin <frankanin99@gmail.com>
2024-08-29 12:58:35 +0000

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """test_client.py"""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -22,3 +22,20 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
             )
+
+    def test_public_repos_url(self):
+        """
+        Test that the result of _public_repos_url is the expected one
+        based on the mocked payload.
+        """
+        known_payload = {
+            "repos_url": "https://api.github.com/orgs/testorg/repos"
+        }
+
+        with patch.object(
+            GithubOrgClient, 'org', new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = known_payload
+            client = GithubOrgClient("testorg")
+            result = client._public_repos_url
+            self.assertEqual(result, known_payload["repos_url"])
