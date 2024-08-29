89f1fc67089830f21761e485abbe296496065439
Frank Anin <frankanin99@gmail.com>
2024-08-29 13:36:35 +0000

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -112,3 +112,15 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def tearDownClass(cls):
         """Tear down class after integration testing."""
         cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """Test the public_repos method."""
+        client = GithubOrgClient("google")
+        repos = client.public_repos()
+        self.assertEqual(repos, self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """Test the public_repos method with a license argument."""
+        client = GithubOrgClient("google")
+        repos = client.public_repos(license="apache-2.0")
+        self.assertEqual(repos, self.apache2_repos)
